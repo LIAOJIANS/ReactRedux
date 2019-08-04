eddac9fe158308a9000c1f84c4848163d04139e5
LIAOJIANS <272781702@qq.com>
2019-08-04 16:42:50 +0800

    redux和react-redux和thunk

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Provider } from 'react-redux'
 
-import App from './comopents/app'
+import App from './containers/app'
+import store from './redux/store'
 
-render(<App/>, document.getElementById('root'))
+render((
+    <Provider store={ store }>
+        <App />
+    </Provider>
+), document.getElementById('root'))
+
